85f8a335bace4cb6232d85a648cfb8131047db03
jamesgichia <jamesgichia15@gmail.com>
2024-11-18 16:28:59 +0300

    Kumaliza vibaya sana

--- portfolio-website/main.go
+++ portfolio-website/main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-    "net/http"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("./static")))
-    http.HandleFunc("/projects", projectsHandler)
-    http.HandleFunc("/contact", contactHandler)
+	// Serve static files (CSS, JS, images)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    http.ListenAndServe(":8080", nil)
+	// Serve the main HTML file
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./index.html")
+	})
+
+	// Start the server
+	port := ":8080"
+	log.Printf("Server started at http://localhost%s", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
 }
-
-func projectsHandler(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "templates/projects.html")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "templates/contact.html")
-}
-
